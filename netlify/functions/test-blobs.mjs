b81e84cf18b7055d5e2b89da9f656062d3c9bbfd
messiaszp <153689952+messiaszp@users.noreply.github.com>
2026-09-13 13:48:07 -0300

    Update test-blobs.mjs

--- netlify/functions/test-blobs.mjs
+++ netlify/functions/test-blobs.mjs
@@ -1,8 +1,21 @@
-{
-  "success": true,
-  "data": {
-    "id": "test",
-    "message": "Netlify Blobs funcionando!",
-    "image": "https://res.cloudinary.com/..."
-  }
-}
+import { getStore } from "@netlify/blobs";
+
+export default async () => {
+
+  const store = getStore("gallery");
+
+  await store.setJSON("test", {
+    id: "test",
+    message: "Netlify Blobs funcionando!",
+    image: "https://res.cloudinary.com/fu66mxxo/image/upload/v1789317517/pc.jpg"
+  });
+
+  const data = await store.get("test", {
+    type: "json"
+  });
+
+  return Response.json({
+    success: true,
+    data
+  });
+};
